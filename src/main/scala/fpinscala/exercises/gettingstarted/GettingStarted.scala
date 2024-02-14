4c1dad6f189ff18061a6848f37b8555eb17755bb
Denys Rybalka <denys.rybalka@tuta.io>
2024-02-14 08:48:07 +0100

    complete chapter 2

--- src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
@@ -1,5 +1,6 @@
 package fpinscala.exercises.gettingstarted
 
+
 // A comment!
 /* Another comment */
 /** A documentation comment */
@@ -20,7 +21,7 @@ object MyProgram:
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if n <= 0 then acc
-      else go(n-1, n*acc)
+      else go(n - 1, n * acc)
 
     go(n, 1)
 
@@ -28,12 +29,18 @@ object MyProgram:
   def factorial2(n: Int): Int =
     var acc = 1
     var i = n
-    while i > 0 do { acc *= i; i -= 1 }
+    while i > 0 do
+      acc *= i; i -= 1
     acc
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int =
+    def loop(n: Int, a: Int, b: Int): Int =
+      if n <= 0 then a
+      else loop(n - 1, b, a + b)
+
+    loop(n, 0, 1)
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) =
@@ -46,6 +53,7 @@ object MyProgram:
     val msg = "The %s of %d is %d."
     msg.format(name, n, f(n))
 
+
 object FormatAbsAndFactorial:
 
   import MyProgram.*
@@ -56,6 +64,7 @@ object FormatAbsAndFactorial:
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
 
+
 object TestFib:
 
   import MyProgram.*
@@ -63,7 +72,18 @@ object TestFib:
   // test implementation of `fib`
   @main def printFib: Unit =
     println("Expected: 0, 1, 1, 2, 3, 5, 8")
-    println("Actual:   %d, %d, %d, %d, %d, %d, %d".format(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5), fib(6)))
+    println(
+      "Actual:   %d, %d, %d, %d, %d, %d, %d".format(
+        fib(0),
+        fib(1),
+        fib(2),
+        fib(3),
+        fib(4),
+        fib(5),
+        fib(6)
+      )
+    )
+
 
 // Functions get passed around so often in FP that it's
 // convenient to have syntax for constructing a function
@@ -80,10 +100,17 @@ object AnonymousFunctions:
     println(formatResult("increment2", 7, (x) => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
-    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+    println(
+      formatResult(
+        "increment5",
+        7,
+        x =>
+          val r = x + 1; r
+      )
+    )
+
 
 object MonomorphicBinarySearch:
-
 
   // First, a findFirst, specialized to `String`.
   // Ideally, we could generalize this to work for any `Array` type.
@@ -100,6 +127,7 @@ object MonomorphicBinarySearch:
       else loop(n + 1) // Otherwise increment `n` and keep looking.
     // Start the loop at the first element of the array.
     loop(0)
+
 
 object PolymorphicFunctions:
 
@@ -121,26 +149,32 @@ object PolymorphicFunctions:
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean =
+    def loop(n: Int): Boolean =
+      if n <= 0 then true
+      else if gt(as(n - 1), as(n)) then false
+      else loop(n - 1)
+
+    loop(as.length - 1)
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A, B) => C): B => C =
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -150,10 +184,9 @@ object PolymorphicFunctions:
   and uncurry and the two forms are in some sense "the same". In FP jargon,
   we say that they are _isomorphic_ ("iso" = same; "morphe" = shape, form),
   a term we inherit from category theory.
-  */
+   */
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
-
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
